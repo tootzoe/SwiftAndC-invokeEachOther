f4e95c93a05e2f60f05bbca118fc5223c6e9dedc
toot_hzf <toot@tootzoe.com>
2024-03-13 14:11:35 +0700

    Initial Commit

---
+++ SwiftAndC-invokeEachOther/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  SwiftAndC-invokeEachOther
+//
+//  Created by thor on 13/3/24
+//  
+//
+//  Email: toot@tootzoe.com  Tel: +855 69325538 
+//
+//
+
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftAndC-invokeEachOther/SwiftAndC_invokeEachOtherApp.swift
@@ -0,0 +1,23 @@
+//
+//  SwiftAndC_invokeEachOtherApp.swift
+//  SwiftAndC-invokeEachOther
+//
+//  Created by thor on 13/3/24
+//  
+//
+//  Email: toot@tootzoe.com  Tel: +855 69325538 
+//
+//
+
+
+
+import SwiftUI
+
+@main
+struct SwiftAndC_invokeEachOtherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
